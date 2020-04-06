c5560f9d9af9e3df038d96a3386e6f52d201c31a
TheForeverLost <alanpjohn@outlook.com>
2020-04-07 00:01:36 +0530

    chrome extension setup

--- PokeAssistant/index.js
+++ PokeAssistant/index.js
@@ -1,1 +1,11 @@
-document.body.style.border =  "5px solid red"
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+          conditions: [new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {hostEquals: 'play.pokemonshowdown.com'},
+          })
+          ],
+              actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+      });
+  });
