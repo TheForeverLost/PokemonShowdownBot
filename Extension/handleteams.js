58ca144d8739681223b4f9f2c7551d912330e9a0
TheForeverLost <alanpjohn@outlook.com>
2020-04-13 01:23:29 +0530

    server setup

--- Extension/handleteams.js
+++ Extension/handleteams.js
@@ -1,13 +1,7 @@
 function onload(){
     let input = document.getElementsByName("pokemon")
     let x  = localStorage.getItem("showdown_teams")
-    x = x.split("]")
-    x.forEach(element => {
-        let li = element.split("|")
-        li.forEach(element2 => {
-            console.log(element2)
-        });
-    });
+    console.log(x)
 }
 onload();
 
